6fb3d744c2355dbb4b1df500a9c195e466b89eac
mingyuan.wei <mingyuan.wei@ymm56.com>
2018-07-05 20:25:23 +0800

    增加对比

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const request = require('request')
+const fs = require('fs-extra')
+const path = require('path')
+const os = require('os')
 require('console.table')
+
+const CACHE_PATH = path.resolve(os.homedir(), '.fx')
 
 async function getInstantPrice (currency, interval = 'M1') {
   if (!currency) return
+  var base = currency[1] || 'N/A'
+  var delta = 'N/A'
+  var direction = 'N/A'
+  currency = currency[0]
   var options = {
     method: 'GET',
     url: 'https://mds-api.forexfactory.com/bars',
@@ -29,22 +38,59 @@ async function getInstantPrice (currency, interval = 'M1') {
       resolve(body.close || 'N/A')
     })
   })
+  if (base && price && base != 'N/A' && price != 'N/A') {
+    delta = +(price - base).toFixed(4)
+    direction = delta > 0 ? '↑' : '↓'
+  }
   return {
     currency,
     price,
+    base,
+    delta,
+    direction
+  }
+}
+
+async function readCache () {
+  var cache
+  try {
+    cache = await fs.readJson(CACHE_PATH, 'utf-8')
+  } catch (e) {
+    cache = {}
+  }
+  return cache
+}
+
+async function writeCache (obj) {
+  var cache = {}
+  try {
+    cache = await fs.readJson(CACHE_PATH, 'utf-8')
+  } catch (e) {}
+  cache = Object.assign(cache, obj)
+  try {
+    await fs.writeJson(CACHE_PATH, cache, 'utf-8')
+  } catch (e) {
+    console.log(e)
   }
 }
 
 async function start () {
+  // let currencies = ['chfjpy', 'usdcad', 'usdjpy']
   let currencies = process.argv.slice(2)
+  let basePrices = {}
+  let cached = await readCache()
   if (currencies.length <= 0) currencies = ['AUD/USD', 'EUR/USD', 'GBP/USD', 'USD/CAD', 'USD/CHF', 'EUR/JPY', 'USD/JPY', 'CHF/JPY', 'AUD/JPY']
   currencies = currencies.map(c => {
-    var pairs = String(c).toUpperCase().match(/^([A-Z]{3})(?:\/?)([A-Z]{3})$/)
-    pairs = pairs && pairs.slice(1) || []
-    return pairs.length == 2 ? pairs.join('/') : null
+    var matched = String(c).toUpperCase().match(/^([A-Z]{3})(?:\/?)([A-Z]{3})(?:\:([\d.]+))?$/)
+    var currency = matched && [matched[1], matched[2]].join('/')
+    if (!/^([A-Z]{3})\/([A-Z]{3})/.test(currency)) return [null, 'N/A']
+    var base = matched && matched[3] || cached[currency] ||  'N/A'
+    if (base && base != 'N/A') cached[currency] = base
+    return [currency, base]
   })
   let prices = await Promise.all(currencies.filter(c => Boolean(c)).map(c => getInstantPrice(c)))
   console.table(prices)
+  await writeCache(cached)
 }
 
 start()
